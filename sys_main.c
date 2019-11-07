85f15abf566fa0a27d9abc8a526f085619885f9c
mahmod ahmed <57040995+mahmodahmed098@users.noreply.github.com>
2019-11-07 21:29:48 +0200

    Add files via upload

--- sys_main.c
+++ sys_main.c
@@ -1,75 +1,50 @@
-/** @file sys_main.c 
-*   @brief Application main file
-*   @date 11-Dec-2018
-*   @version 04.07.01
-*
-*   This file contains an empty main function,
-*   which can be used for the application.
-*/
+/
 
-/* 
-* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
-* 
-* 
-*  Redistribution and use in source and binary forms, with or without 
-*  modification, are permitted provided that the following conditions 
-*  are met:
-*
-*    Redistributions of source code must retain the above copyright 
-*    notice, this list of conditions and the following disclaimer.
-*
-*    Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the 
-*    documentation and/or other materials provided with the   
-*    distribution.
-*
-*    Neither the name of Texas Instruments Incorporated nor the names of
-*    its contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
+/*
+ *     *pref:      First code to learn how can i use ADC module With HERCULES launchpad based RM42L432 "TASK ADC"
+ *     *Auther:    MAHMOUD AHMED ABD_elghany
+ *     *Date:      7-11-2019
+ *     *file name: ADC 2
+ *
+ */
 
 
-/* USER CODE BEGIN (0) */
-/* USER CODE END */
 
-/* Include Files */
 
-#include "sys_common.h"
+   #include "sys_common.h"
+   #include "adc.h"
 
-/* USER CODE BEGIN (1) */
-/* USER CODE END */
+   adcData_t adc_data;
+   adcData_t *adc_data_ptr = &adc_data;
 
-/** @fn void main(void)
-*   @brief Application main function
-*   @note This function is empty by default.
-*
-*   This function is called after startup.
-*   The user can use this function to implement the application.
-*/
+   unsigned int reading ;
 
-/* USER CODE BEGIN (2) */
-/* USER CODE END */
 
-int main(void)
-{
-/* USER CODE BEGIN (3) */
-/* USER CODE END */
+
+
+
+
+
+    int main(void)
+   {
+        adcInit();
+        while(1){
+
+             adcStartConversion(adcREG1 , 1U);
+             while( !adcIsConversionComplete(adcREG1, 1U));
+             adcGetData(adcREG1, 1U , adc_data_ptr);
+             reading = adc_data_ptr->value ;
+        }
+
+
+
 
     return 0;
-}
+   }
+
+
+
+
 
 
 /* USER CODE BEGIN (4) */
